340d35f21dd63f14338b63cb5e648645e646f278
carlosb134 <carbuc2@alu.edu.gva.es>
2024-11-22 11:03:21 +0100

    Strings

--- src/main/sumabinario.java
+++ src/main/sumabinario.java
@@ -1,38 +1,29 @@
 
 public class sumabinario {
-    public static void main(String[] args) {
-        String num1 = "1010";
-        String num2 = "1101";
-
-        String suma = sumarBinario(num1, num2);
-        System.out.println("Suma binaria: " + suma);
-    }
-
-    public static String sumarBinario(String num1, String num2) {
+    public static String sumaBinario(String binario1, String binario2) {
         String res = "";
-
-        int DigitoA = Integer.parseInt(String.valueOf(num1.charAt(0)));
+        int digitoA = Integer.parseInt("" + binario1.charAt(0));
         char b1;
         char b2;
         int bit1;
         int bit2;
-        int acarreo = 0;
         int suma;
-
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            b1 = num1.charAt(i);
-            b2 = num2.charAt(i);
-            bit1 = Integer.parseInt(String.valueOf(b1));
-            bit2 = Integer.parseInt(String.valueOf(b2));
-
+        int acarreo = 0;
+        for (int i = binario1.length() - 1; i >= 0; i--) {
+            b1 = binario1.charAt(i);
+            b2 = binario2.charAt(i);
+            bit1 = Integer.parseInt("" + b1);
+            bit2 = Integer.parseInt("" + b2);
             suma = bit1 + bit2 + acarreo;
-
             if (suma == 0) {
                 res = "0" + res;
                 acarreo = 0;
             } else if (suma == 1) {
                 res = "1" + res;
                 acarreo = 0;
+            } else if (suma == 2) {
+                res = "0" + res;
+                acarreo = 1;
             } else {
                 res = "1" + res;
                 acarreo = 1;
@@ -42,5 +33,8 @@ public class sumabinario {
             res = "1" + res;
         }
         return res;
+    }
+    public static void main(String[] args) {
+        System.out.println(sumaBinario("1101", "1010"));
     }
 }
